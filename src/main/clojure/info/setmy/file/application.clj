00872dc5fbaff8855e40d101a740f0c77a4bc283
Imre Tabur <imre.tabur@mail.ee>
2023-08-30 18:21:52 +0300

    Configuration related work started vol 2

--- src/main/clojure/info/setmy/file/application.clj
+++ src/main/clojure/info/setmy/file/application.clj
@@ -1,2 +1,32 @@
 (ns info.setmy.file.application
-    "Application yaml/properties/toml representing functionality.")
+    "Application yaml/properties/toml representing functionality."
+    (:require [java.util Properties]))
+
+(def application-properties-file-name "application.properties")
+(def application-yaml-file-name "application.yaml")
+
+(defonce parsed-application-properties-atom (atom nil))
+
+(defn get-application-properties []
+    @parsed-application-properties-atom)
+
+(defn set-application-properties [properties]
+    (when (nil? @parsed-application-properties-atom)
+          (swap! parsed-application-properties-atom properties))
+    @parsed-application-properties-atom)
+
+(defn load-properties-from-classpath [filename]
+    (let [classloader (Thread/currentThread)
+          .getContextClassLoader
+          properties
+          (Properties.)]
+        (with-open [stream (.getResourceAsStream classloader filename)]
+            (if stream
+                (do
+                    (.load properties stream)
+                    properties)
+                (throw (Exception. (str "Faili " filename " ei leitud CLASSPATH-ilt")))))))
+
+
+(defn init []
+    (load-properties-from-classpath application-properties-file-name))
--- src/main/clojure/info/setmy/file/config.clj
+++ src/main/clojure/info/setmy/file/config.clj
@@ -1,3 +1,9 @@
 (ns info.setmy.file.config
     "Configuration getting (from CLI, env, yaml) functionality. Central pint to store configuration values for other software parts."
-    (:gen-class))
+    (:gen-class)
+    (:require [info.setmy.file.application :as application]
+              [info.setmy.file.cli :as cli]
+              [info.setmy.file.env :as env]))
+
+(defn init []
+    (application/init))
